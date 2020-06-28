af2a164469d13822265488d195bd2ee6a544aa5d
Dmitriy Khayzin <dkhayzin@gmail.com>
2020-06-28 12:15:12 -0400

    small refactor/simplification of code; separating current line from past lines for performance enhancement and ease of use; adding more edge case handling

--- src/Canvas.js
+++ src/Canvas.js
@@ -3,52 +3,83 @@ import { Stage, Layer, Rect, Text, Line } from 'react-konva';
 import Konva from 'konva';
 
 const COLORS = ['red', 'blue', 'green', 'yellow'];
-const INTERVAL = 10;
+const INTERVAL = 20;
 
 export default function Canvas(props) {
-  const [timer, setTimer] = useState(null);
-  const [drawPoints, setDrawPoints] = useState([]);
+  const [timer, setTimer] = useState(null);  // does not need to be a hook
+  const [currentLine, setCurrentLine] = useState(null);
+  const [previousLines, setPreviousLines] = useState([]);
 
-  const getPointerPosition = stage =>
-    setTimer(
-      setInterval(() => {
-        const {x, y} = stage.getPointerPosition();
-        setDrawPoints((drawPoints) => {
-          const lastLine = drawPoints[drawPoints.length - 1];
-          const newPoints = lastLine.points.concat([x, y]);
-          // replacing with a new array as opposed to using the original,
-          // to ensure that the reference is not mutated outside of the state hook
-          lastLine.points = newPoints;
-          return [ ...drawPoints ]; // return shallow clone
-        });
-      }, INTERVAL)
-    );
+  const startDrawing = (stage) => {
+    console.log('start');
 
-  const handleDragToggle = (e, isDragging = false) => {
-    // TODO: check for out of bounds dragging
-    if (isDragging) {
-      const stage = e.target.getStage();
-      // create a new line with color
-      setDrawPoints(drawPoints => drawPoints.concat([
-        {
-          color: COLORS[Math.floor(Math.random() * COLORS.length)],
-          points: []
-        }
-      ]));
-      getPointerPosition(stage);
-    } else {
+    // clear an old timer, if one exists for any reason
+    stopDrawing();
+
+    const newTimer = setInterval(() => {
+      const { x, y } = stage.getPointerPosition(); // gives x, y coords from the stage
+
+
+      setCurrentLine((currentLine) => {
+        // only add a new point if the user actually moved the mouse
+        const [prevX, prevY] = currentLine.points.slice(-2);
+
+        return prevX === x && prevY === y ?
+          currentLine :
+          { ...currentLine, points: currentLine.points.concat([x, y]) }
+      });
+    }, INTERVAL);
+
+    setTimer(newTimer);
+  }
+
+  const stopDrawing = () => {
+    if(timer) {
+      console.log('stop');
+      setPreviousLines(previousLines.concat([currentLine]));
       clearInterval(timer);
-      console.log('stop interval');
+      setTimer(null);
+      setCurrentLine(null);
+      console.log(previousLines);
     }
   }
 
-  console.log(drawPoints);
+  const handleDragToggle = (e, isDrawing = false) => {
+    // TODO: check for out of bounds dragging
+    if (isDrawing) {
+      const stage = e.target.getStage();
+      // create a new line with color
+      setCurrentLine({
+        color: COLORS[Math.floor(Math.random() * COLORS.length)],
+        points: []
+      });
+      startDrawing(stage);
+    } else {
+      stopDrawing();
+    }
+  }
+
+  const handleMouseEnter = (e) => {
+    if(e.evt.buttons === 1) { // left click is currently pressed
+      const stage = e.target.getStage();
+      // create a new line with color
+      const { x, y } = stage.getPointerPosition()
+      setCurrentLine({
+        color: COLORS[Math.floor(Math.random() * COLORS.length)],
+        points: [x, y]
+      });
+      startDrawing(stage);
+    }
+  }
+  // console.log(currentLine);
 
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer
         onMouseDown={e => handleDragToggle(e, true)}
         onMouseUp={e => handleDragToggle(e, false)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={e => stopDrawing()}
       >
         <Rect
           x={20}
@@ -58,7 +89,9 @@ export default function Canvas(props) {
           fill={'white'}
           shadowBlur={5}
         />
-        { drawPoints.map(line => (
+
+        {/* old lines */}
+        { previousLines.map(line => (
           <Line
             points={line.points}
             stroke={line.color}
@@ -69,6 +102,16 @@ export default function Canvas(props) {
           />
         )) }
 
+        {/* current line */}
+        { currentLine && (<Line
+            points={currentLine.points}
+            stroke={currentLine.color}
+            strokeWidth={5}
+            lineCap={'round'}
+            lineJoin={'round'}
+            tension={1}
+          />)
+        }
       </Layer>
     </Stage>
   )
